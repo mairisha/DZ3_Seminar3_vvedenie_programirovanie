a75dd34ad27de6f89194fe57fd79540518cdfe31
Ирина <mairisha@yandex.ru>
2022-10-10 13:02:38 +0300

    Zadacha 1

---
+++ DZ3-Zadacha1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число N: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int cube=1;
+int count=1;
+while (count<=N)
+{
+    cube=count*count*count;
+    Console.WriteLine($"Куб от числа {count} равен {cube}");
+    count=count+1;
+}
