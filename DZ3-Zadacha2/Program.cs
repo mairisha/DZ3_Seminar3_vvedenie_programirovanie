1f284e5d3074a9dbde432b0b219f9bd4ca10e28b
Ирина <mairisha@yandex.ru>
2022-10-13 14:13:15 +0300

    Zadacha2

---
+++ DZ3-Zadacha2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum=0;
+while (number>0)
+{
+    sum=sum+(number%10);
+    number=number/10;
+}
+Console.WriteLine(sum);
