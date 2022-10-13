d9225694bc2662dd8691157f47944edfa5e63306
Ирина <mairisha@yandex.ru>
2022-10-13 14:14:54 +0300

    Zadacha4

---
+++ DZ3-Zadacha4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число N");
+int N=Convert.ToInt32(Console.ReadLine());
+int a=1;
+int cube=1;
+while (a<=N)
+{
+   if (a%2==0)
+{
+  cube=a*a*a;
+  Console.WriteLine($"Куб от числа {a} = {cube}");
+}
+a=a+1;
+}
