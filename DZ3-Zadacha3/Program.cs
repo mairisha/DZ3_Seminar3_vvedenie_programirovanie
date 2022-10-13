ceb85025e40480152a8fd2628bb4e9745701bd93
Ирина <mairisha@yandex.ru>
2022-10-13 14:14:06 +0300

    Zadacha3

---
+++ DZ3-Zadacha3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число ");
+int N=Convert.ToInt32(Console.ReadLine());
+int comp=1;
+int count=1;
+while (count<N)
+{
+    comp=comp*count;
+    count=count++;
+} 
+    count=count*N;
+    Console.WriteLine(count);
